9df00eac6e8a17316b20b2c80004be47892d2aba
okuzawats <okuzawats@gmail.com>
2022-10-05 22:22:00 +0900

    add type

--- app/src/test/java/com/okuzawats/truth/StringTest.kt
+++ app/src/test/java/com/okuzawats/truth/StringTest.kt
@@ -6,14 +6,14 @@ import org.junit.Test
 class StringTest {
   @Test
   fun test_isEqualTo() {
-    val actual = "hello world"
+    val actual: String = "hello world"
     assertThat(actual)
       .isEqualTo("hello world")
   }
 
   @Test
   fun test_ignoreCase_isEqualTo() {
-    val actual = "hello world"
+    val actual: String = "hello world"
     assertThat(actual)
       .ignoringCase()
       .isEqualTo("HELLO WORLD")
@@ -21,63 +21,63 @@ class StringTest {
 
   @Test
   fun test_hasLength() {
-    val actual = "hello world"
+    val actual: String = "hello world"
     assertThat(actual)
       .hasLength(11)
   }
 
   @Test
   fun test_startsWith() {
-    val actual = "hello world"
+    val actual: String = "hello world"
     assertThat(actual)
       .startsWith("hello")
   }
 
   @Test
   fun test_endsWith() {
-    val actual = "hello world"
+    val actual: String = "hello world"
     assertThat(actual)
       .endsWith("world")
   }
 
   @Test
   fun test_isEmpty() {
-    val actual = ""
+    val actual: String = ""
     assertThat(actual)
       .isEmpty()
   }
 
   @Test
   fun test_isNotEmpty() {
-    val actual = "hello world"
+    val actual: String = "hello world"
     assertThat(actual)
       .isNotEmpty()
   }
 
   @Test
   fun test_contains() {
-    val actual = "hello world"
+    val actual: String = "hello world"
     assertThat(actual)
       .contains("wor")
   }
 
   @Test
   fun test_doesNotContains() {
-    val actual = "hello world"
+    val actual: String = "hello world"
     assertThat(actual)
       .doesNotContain("zzz")
   }
 
   @Test
   fun test_containsMatch() {
-    val actual = "hello world"
+    val actual: String = "hello world"
     assertThat(actual)
       .containsMatch("^hello [a-z]+")
   }
 
   @Test
   fun testDoesNotContainMatch() {
-    val actual = "hello world"
+    val actual: String = "hello world"
     assertThat(actual)
       .doesNotContainMatch("^[A-Z]+")
   }
