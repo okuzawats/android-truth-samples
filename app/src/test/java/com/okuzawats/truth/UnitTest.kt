07a4c60664f8b51ca435a999e7114bc0763f46ed
okuzawats <okuzawats@gmail.com>
2023-03-25 23:10:31 +0900

    implement UnitSubject

---
+++ app/src/test/java/com/okuzawats/truth/UnitTest.kt
@@ -0,0 +1,18 @@
+package com.okuzawats.truth
+
+import com.okuzawats.truth.UnitSubject.assertThat
+import org.junit.Test
+
+class UnitTest {
+  @Test
+  fun test_isNotNull() {
+    val actual: Unit? = null
+    assertThat(actual).isNotUnit()
+  }
+
+  @Test
+  fun test_isNull() {
+    val actual: Unit? = Unit
+    assertThat(actual).isUnit()
+  }
+}
