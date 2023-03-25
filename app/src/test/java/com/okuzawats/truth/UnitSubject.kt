88464f0511a638f88d23d4bbeda55894c31ee425
okuzawats <okuzawats@gmail.com>
2023-03-25 23:19:20 +0900

    reformat UnitSubject

--- app/src/test/java/com/okuzawats/truth/UnitSubject.kt
+++ app/src/test/java/com/okuzawats/truth/UnitSubject.kt
@@ -4,30 +4,38 @@ import com.google.common.truth.FailureMetadata
 import com.google.common.truth.Subject
 import com.google.common.truth.Subject.Factory
 import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertAbout
 
-class UnitSubject private constructor(metadata: FailureMetadata, private val actual: Unit?) :
-    Subject(metadata, actual) {
+class UnitSubject private constructor(
+  metadata: FailureMetadata,
+  private val actual: Unit?,
+) : Subject(metadata, actual) {
 
-    fun isUnit() =  unit().isEqualTo(Unit)
+  /**
+   * assert that actual is Unit.
+   */
+  fun isUnit() = unit().isEqualTo(Unit)
 
-    fun isNotUnit() = unit().isNotEqualTo(Unit)
+  /**
+   * assert that actual is NOT Unit.
+   */
+  fun isNotUnit() = unit().isNotEqualTo(Unit)
 
-    fun unit(): Subject {
-        return check("name()").that(actual)
+  private fun unit(): Subject = check("unit()").that(actual)
+
+  companion object {
+    /**
+     * start assertion of object with type Unit?
+     */
+    fun assertThat(actual: Unit?): UnitSubject = assertAbout(unit()).that(actual)
+
+    private fun unit(): Factory<UnitSubject, Unit?> {
+      return Factory { metadata: FailureMetadata, subject: Unit? ->
+        UnitSubject(
+          metadata,
+          subject,
+        )
+      }
     }
-
-    companion object {
-        fun assertThat(actual: Unit?): UnitSubject {
-            return Truth.assertAbout(employees()).that(actual)
-        }
-
-        fun employees(): Factory<UnitSubject, Unit?> {
-            return Factory { metadata: FailureMetadata, subject: Unit? ->
-                UnitSubject(
-                    metadata,
-                    subject
-                )
-            }
-        }
-    }
+  }
 }
