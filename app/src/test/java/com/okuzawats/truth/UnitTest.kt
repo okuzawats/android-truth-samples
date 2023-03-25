97576ebee27a8bd1a5ed897f439fbb69c6327f5d
okuzawats <okuzawats@gmail.com>
2023-03-25 23:32:39 +0900

    fix naming of test case

--- app/src/test/java/com/okuzawats/truth/UnitTest.kt
+++ app/src/test/java/com/okuzawats/truth/UnitTest.kt
@@ -5,13 +5,13 @@ import org.junit.Test
 
 class UnitTest {
   @Test
-  fun test_isNotNull() {
+  fun test_isNotUnit() {
     val actual: Unit? = null
     assertThat(actual).isNotUnit()
   }
 
   @Test
-  fun test_isNull() {
+  fun test_isUnit() {
     val actual: Unit? = Unit
     assertThat(actual).isUnit()
   }
